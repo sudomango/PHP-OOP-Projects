80873b0e70311e340197990568bc9a32cf951ef7
sudomango <mark.saburov.work@gmail.com>
2023-11-06 14:06:02 +0000

    Встроенная библиотека iconv заменена на multibyte string

--- unicode_string.php
+++ unicode_string.php
@@ -14,53 +14,41 @@ class Ustring
 
     function at(int $index): string
     {
-        $length = iconv_strlen($this->content, "UTF-8");
+        $length = mb_strlen($this->content, "UTF-8");
         if ($index >= $length) $index %= $length;
         if ($index < 0) {
             while ($index < 0) $index += $length;
         }
-        return iconv_substr($this->content, $index, 1, "UTF-8");
+        return mb_substr($this->content, $index, 1, "UTF-8");
     }
 
     function has(string $substring): bool
     {
-        $result = iconv_strpos($this->content, $substring, 0, "UTF-8");
+        $result = mb_strpos($this->content, $substring, 0, "UTF-8");
         return is_integer($result) ? True : False;
     }
 
     function count(string $substring): int
     {
-        $count = 0;
-        $index = 0;
-        $length = iconv_strlen($this->content, "UTF-8");
-        $length_substr = iconv_strlen($substring, "UTF-8");
-        
-        while ($index < $length) {
-            $result = iconv_strpos($this->content, $substring, $index, "UTF-8");
-            if (is_integer($result)) {
-                $count += 1;
-                $index = $result + $length_substr + 1;
-            } else {
-                $index = $length; # Можно также выйти из цикла при помощи инструкции break.
-            }
-        }
-
-        return $count;
+        # В качестве альтернативы можно воспользоваться встроенной функцией mb_substr_count.
+        $result_array = [];
+        preg_match_all("/$substring/misu", $this->content, $result_array);
+        return count($result_array);
     }
 
     function length(): int
     {
-        return iconv_strlen($this->content, "UTF-8");
+        return mb_strlen($this->content, "UTF-8");
     }
 
     function index(string $substring, int $position): int
     {
-        return iconv_strpos($this->content, $substring, $position, "UTF-8");
+        return mb_strpos($this->content, $substring, $position, "UTF-8");
     }
 
     function rindex(string $substring, int $position): int
     {
-        return iconv_strrpos($this->content, $substring, $position, "UTF-8");
+        return mb_strrpos($this->content, $substring, $position, "UTF-8");
     }
 
     function lfind(string $substring, int $position): int
