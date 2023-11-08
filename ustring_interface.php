6e28b6849e410ad7035b5297187d67cf5347b529
sudomango <mark.saburov.work@gmail.com>
2023-11-08 15:20:01 +0000

    Дополнен и обновлён интерфейс класса Ustring

--- ustring_interface.php
+++ ustring_interface.php
@@ -2,23 +2,27 @@
 
 interface UstringInterface
 {
-    public function reverse();
-    public function substr();
-    public function has();
-    public function count();
-    public function split();
-    public function at();
-    public function replace();
+    public function reverse(): string;
+    public function substr(int $start_index, int $length): string;
+    public function slice(int $start_index, int $length): string;
+    public function at(int $index): string;
+    public function has(string $substring): bool;
+    public function count(string $substring): int;
+    public function length(): int;
+    public function split(): array;
+    public function replace(string $find, string $replace, bool $ignore_case = False): string;
 
-    public function index();
-    public function lfind();
-    public function rfind();
+    public function index(string $substring, int $position = 0): int;
+    public function rindex(string $substring, int $position = -1): int;
+    public function lfind(string $substring, int $position = 0): int;
+    public function rfind(string $substring, int $position = -1): int;
 
-    public function lower();
-    public function upper();
+    public function lower(): string;
+    public function upper(): string;
 
-    public function isalpha();
-    public function isdigit();
-    public function isalnum();
-    public function isnumeric();
+    public function isalpha(): bool;
+    public function isalnum(): bool;
+    public function isdigit(): bool;
+    public function isinteger(): bool;
+    public function isnumber(): bool;
 }
